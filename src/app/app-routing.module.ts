0ed0dee3f9ab6907b0031e33a30421da81fb9266
DonTheChimera <donaldtbradfield@gmail.com>
2019-03-19 11:46:37 -0500

    viewport skeleton

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { WelcomeComponent } from './main/welcome/welcome.component';
+
+const routes: Routes = [
+  { path: '', component: WelcomeComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sample-dashboard';
+
+  sideMessage: string = 'hide';
+
+  ngOnInit() {}
+
+  constructor() { }
+
+  showSidenav: boolean = true;
+
+  onSideNav() {
+    this.showSidenav = !this.showSidenav;
+    if (this.showSidenav) {
+      this.sideMessage = 'hide'
+    } else {this.sideMessage = 'show'}
+  }
+
+
 }
